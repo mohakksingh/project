bffd4fb9ed0bfc1dfe0e12d962fabf4ee5099e6a
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-21 20:40:33 +0530

    Charts Completed

--- admin-dashboard/src/pages/charts/LineCharts.tsx
+++ admin-dashboard/src/pages/charts/LineCharts.tsx
@@ -1,5 +1,5 @@
-import AdminSidebar from "../../components/AdminSidebar"
-import { LineChart } from "../../components/Charts"
+import AdminSidebar from "../../components/AdminSidebar";
+import { LineChart } from "../../components/Charts";
 
 const months = [
   "Jan",
@@ -17,14 +17,16 @@ const months = [
 ];
 
 const LineCharts = () => {
-    return (
-        <div className="admin-container">
-        <AdminSidebar/>
-        <main className="chart-container">
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main className="chart-container">
         <h1>Line Charts</h1>
         <section>
           <LineChart
-            data={[200, 444, 343, 556, 778, 455, 990,144,256,447,1000,1200]}
+            data={[
+              200, 444, 343, 556, 778, 455, 990, 144, 256, 447, 1000, 1200,
+            ]}
             label="Users"
             borderColor="rgb(53,162,255)"
             backgroundColor="rgba(53,162,255,0.5)"
@@ -32,9 +34,45 @@ const LineCharts = () => {
           />
           <h2>Active Users</h2>
         </section>
+        <section>
+        <LineChart
+            data={[40, 60, 244, 100, 143, 120, 41, 47, 50, 56, 32]}
+            backgroundColor={"hsla(269,80%,40%,0.4)"}
+            borderColor={"hsl(269,80%,40%)"}
+            label="Products"
+            labels={months}
+          />
+          <h2>Total Products (SKU)</h2>
+        </section>
+        <section>
+          <LineChart
+            data={[
+              24000, 14400, 24100, 34300, 90000, 20000, 25600, 44700, 99000,
+              144400, 100000, 120000,
+            ]}
+            backgroundColor={"hsla(129,80%,40%,0.4)"}
+            borderColor={"hsl(129,80%,40%)"}
+            label="Revenue"
+            labels={months}
+          />
+          <h2>Total Revenue</h2>
+        </section>
+        <section>
+          <LineChart
+            data={[
+              9000, 12000, 12000, 9000, 1000, 5000, 4000, 1200, 1100, 1500,
+              2000, 5000,
+            ]}
+            backgroundColor={"hsla(29,80%,40%,0.4)"}
+            borderColor={"hsl(29,80%,40%)"}
+            label="Discount"
+            labels={months}
+          />
+          <h2>Discount Allotted</h2>
+        </section>
       </main>
-      </div>
-      )
-}
+    </div>
+  );
+};
 
-export default LineCharts
+export default LineCharts;
