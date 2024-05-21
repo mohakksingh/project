04e7f70558f529694c88e24a31627f5b21540186
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-21 05:32:58 +0530

    pie chart for stock availability (5:11)

--- admin-dashboard/src/components/Charts.tsx
+++ admin-dashboard/src/components/Charts.tsx
@@ -148,3 +148,41 @@ export const DoughnutChart = ({
 
   return <Doughnut data={doughnutData} options={doughnutOptions} />;
 };
+
+interface PieChartProps {
+  labels: string[];
+  data: number[];
+  backgroundColor: string[];
+  offset?: number[];
+}
+
+export const PieChart = ({
+  labels,
+  data,
+  backgroundColor,
+  offset,
+}: PieChartProps) => {
+  const pieChartData: ChartData<"pie", number[], string> = {
+    labels,
+    datasets: [
+      {
+        data,
+        backgroundColor,
+        borderWidth: 1,
+        offset,
+      },
+    ],
+  };
+
+  const pieChartOptions: ChartOptions<"pie"> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  return <Pie data={pieChartData} options={pieChartOptions} />;
+};
+
--- admin-dashboard/src/pages/charts/PieCharts.tsx
+++ admin-dashboard/src/pages/charts/PieCharts.tsx
@@ -1,13 +1,43 @@
-import AdminSidebar from "../../components/AdminSidebar"
-
+import AdminSidebar from "../../components/AdminSidebar";
+import { DoughnutChart, PieChart } from "../../components/Charts";
+import { categories } from "../../assets/data.json";
 
 const PieCharts = () => {
-    return (
-        <div className="admin-container">
-        <AdminSidebar/>
-        <main>hi</main>
-      </div>
-      )
-}
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main className="chart-container">
+        <h1>Pie & Doughnut Charts</h1>
+        <section>
+          <div>
+            <PieChart
+              labels={["Processing", "Shipped", "Delivered"]}
+              data={[12, 9, 13]}
+              backgroundColor={[
+                `hsl(110,80%,80%)`,
+                `hsl(110,80%,50%)`,
+                `hsl(110,40%,50%)`,
+              ]}
+              offset={[0, 0, 50]}
+            />
+          </div>
+          <h2>Order Fullfillment Ratio</h2>
+        </section>
+        <section>
+          <div>
+            <DoughnutChart
+            labels={categories.map((i) => i.heading)}
+            data={categories.map((i) => i.value)}
+              backgroundColor={categories.map(i=>`hsl(${i.value*4},${i.value}%,50%)`)}
+              legends={false}
+              offset={[0, 0, 0,80]}
+            />
+          </div>
+          <h2>Product Category Ratio</h2>
+        </section>
+      </main>
+    </div>
+  );
+};
 
-export default PieCharts
+export default PieCharts;
